e0a1523f25584cf2c613079a3f094f6f1d5e70c3
Alexandra <oleksandra.kryvko@nure.ua>
2020-04-25 17:44:14 +0300

    Implement manage users

---
+++ Evento/Evento.Web/Models/Users/EditUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Evento.Web.Models.Users
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DataOfBirth { get; set; }
+
+        public string Email { get; set; }
+    }
+}
