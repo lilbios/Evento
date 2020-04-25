3f3ef205d8b889beacd8d5621a5f74e0ad702ece
Alexandra <oleksandra.kryvko@nure.ua>
2020-04-25 15:04:34 +0300

    Implement Repository, UnitOfWork, IUnitOfWork

--- Evento/Evento.DTO/Repositories/IUnitOfWork.cs
+++ Evento/Evento.DTO/Repositories/IUnitOfWork.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Evento.DTO.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Evento.DTO.Repositories
 {
     public interface IUnitOfWork
     {
+        IRepository<User> Users { get; }
+
+        IRepository<Event> Events { get; }
+
+        IRepository<Subscription> Subscriptions { get; }
+
+        IRepository<Memorize> Memorizes { get; }
+
+        IRepository<Comment> Comments { get; }
+
+        IRepository<Location> Locations { get; }
+
+        IRepository<Category> Categories { get; }
+
+        IRepository<Tag> Tags { get; }
+
+        Task SaveChangesAsync();
     }
 }
